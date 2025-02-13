b582c07f38bce07265ecbc2186391b14519ae54b
ergashevbehruzjon <ergashevbehruzjon@gmail.com>
2025-02-13 09:06:06 +0500

    Initial Commit

---
+++ OOP-fifth/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  OOP-fifth
+//
+//  Created by Behruzjon Ergashev on 13/02/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
